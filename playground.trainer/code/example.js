23f6da79f97b94c08d46cfb1ad85e5d1cd9148bb
leegeunseong <skscp223@gmail.com>
2019-10-01 21:02:52 +0900

    backend example add

---
+++ playground.trainer/code/example.js
@@ -0,0 +1,12 @@
+module.exports.function = function example (example) { // action의 input = js의 parameter
+  var ex1 = []; // = java static
+  const ex2 = []; // = java final
+  let ex3 = []; // = local variable
+
+  // https://bixbydevelopers.com/dev/docs/reference/JavaScriptAPI 참고
+  // const config = require('config');
+  // const http = require('http');
+  // const fail = require('fail');
+
+  return example; // action outpt = js return
+}
